78349501fdcf7653e07043d79cbb598b7351cd68
David Diaz <david.diaz@zweicom.com>
2018-11-27 16:13:54 -0300

    se agregan rutas de componentes en index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,20 @@
-export { default as Information } from './Information'
-export { default as InformationWelcome } from './InformationWelcome'
-export { default as InformationLogin } from './InformationLogin'
-export { default as Header } from './Header'
 export { default as HorizontalMenu } from './HorizontalMenu'
-export { default as Contenido } from './Contenido'
+export { default as Info } from './Info'
+export { default as Header } from './Header'
+export { default as Login } from './Login'
+
+
+export { default as Products } from './Products'
+export { default as Promotions } from './Promotions'
+export { default as Contact } from './Contact'
+export { default as ShoopingCartProducts } from './ShoopingCartProducts'
+export { default as MyOrders } from './MyOrders'
+
+
 export { default as Footer } from './Footer'
 
 
+//Otros
+export { default as Information } from './Information'
+export { default as InformationWelcome } from './InformationWelcome'
+export { default as InformationLogin } from './InformationLogin'
